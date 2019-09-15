660de1a7464971ecad5656a0efbad405505680e6
Andrey Chernoprudov <dinloq@gmail.com>
2019-09-15 20:05:29 +0500

    photon painter

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:light_speed/src/widget/photon_painter.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Light speed'),),
-      body: Container(),);
+      appBar: AppBar(
+        title: Text('Light speed'),
+      ),
+      body: Container(
+        color: Colors.blue,
+        width: double.maxFinite,
+        height: 200,
+        child: CustomPaint(
+          painter: PhotonPainter(0.9),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/src/widget/photon_painter.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class PhotonPainter extends CustomPainter {
+  final double position; // from 0.0 to 1.0
+
+  PhotonPainter(this.position);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var y = size.height / 2;
+    var x = size.width * position;
+    var paint = Paint()
+      ..style = PaintingStyle.fill
+      ..color = Colors.white
+      ..isAntiAlias = true;
+
+    var center = Offset(x, y);
+    canvas.drawCircle(center, 5, paint);
+
+    canvas.save();
+    canvas.restore();
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
