fdfe5be3fcde475422ca19e0b2eb19c1ef56c627
Andrey Chernoprudov <dinloq@gmail.com>
2019-09-15 21:23:08 +0500

    light item

---
+++ lib/src/models/light_item.dart
@@ -0,0 +1,10 @@
+class LightItem {
+  final String leadingTitle;
+  final String leadingImageUrl;
+  final String trailingTitle;
+  final String trailingImageUrl;
+  final int durationMills;
+
+  LightItem(this.leadingTitle, this.leadingImageUrl, this.trailingTitle,
+      this.trailingImageUrl, this.durationMills);
+}
